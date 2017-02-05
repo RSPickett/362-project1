aaf32be5cd09ebb34d0c26cca8289d9c5db94deb
Chip Pickett <rpickett@hawk.iit.edu>
2017-02-05 09:48:06 -0600

    Added noConflic and function to check valid email address

---
+++ script/style.js
@@ -0,0 +1,19 @@
+// Tell jQuery to give up the $(dollar) sign
+$.noConflict();
+
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+
+  $('#email').on('keyup', function() {
+    var currentValue = $(this);
+    var validPattern = $(this).attr('pattern');
+    console.log(currentValue, validPattern);
+    if(currentValue.match(validPattern)) {
+      $('#submit').addClass('active');
+    else {
+      $('#submit').removeClass('active');
+    }
+    }
+  })
+})
