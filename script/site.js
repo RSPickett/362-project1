437bb94adfd022c7aa6acb7baaa367c18e408aed
Chip Pickett <rpickett@hawk.iit.edu>
2017-02-13 19:03:24 -0600

    added code to implement an accordian function

--- script/site.js
+++ script/site.js
@@ -1,19 +1,23 @@
 
-$.noConflict();
 
-jQuery(function($) {
-  $("html").removeClass("nojs");
-  $("html").addClass("hasjs");
-
-  $("#email").on("keyup", function() {
-    var currentValue = $("this");
-    var validPattern = $("this").attr("pattern");
-    console.log(currentValue, validPattern);
-    if(currentValue.match(validPattern)) {
-      $("#submit").addClass("active");
-    }
-    else {
-      $("#submit").removeClass("active");
-    }
+$(function () {
+  $('.river-definition').on('click', function (e) {
+    e.preventDefault();
+    $(this).parent('.river-info').children('.river-description:visible').slideUp('slow');
+    $(this).nextUntil('.river-definition').filter(':not(:visible)').slideDown('slow');
   });
 });
+
+// Tell jQuery to give up the dollar sign
+// $.noConflict();
+// Instead, use a self-invoking function to have a private
+// dollar sign
+// (function($){
+//   $(document).ready(
+//     function() {
+//       $('html').removeClass('nojs');
+//       $('html').addClass('hasjs');
+//     }
+//   );
+// })(jQuery);
+
