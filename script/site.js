189539e3797168555849661ad3dc2d2f14b99470
Chip Pickett <rpickett@hawk.iit.edu>
2017-03-03 12:07:34 -0600

    added javascript to float in the submit button

--- script/site.js
+++ script/site.js
@@ -13,6 +13,19 @@ $(function () {
     $(this).parent('.river-info').children('.river-description:visible').slideUp('slow');
     $(this).nextUntil('.river-definition').filter(':not(:visible)').slideDown('slow');
   });
+
+  $('#email').on('keyup focus blur', function() {
+    var currentValue = $(this).val();
+    var validPattern = $(this).attr('pattern');
+    console.log(currentValue, validPattern);
+    if(currentValue.match(validPattern)) {
+      $('#submit').addClass('active');
+      console.log("true");
+    } else {
+      $('#submit').removeClass('active');
+      console.log("false");
+    }
+  });
 });
 
 // Tell jQuery to give up the dollar sign
